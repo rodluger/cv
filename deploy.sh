a3b84ec942a600cd3259a86da0daf50e91fb2fb3
Rodrigo Luger <rodluger@gmail.com>
2017-09-05 15:35:45 -0700

    pdf a la dfm

--- deploy.sh
+++ deploy.sh
@@ -12,22 +12,9 @@ echo "Committing pdf..."
 rev=$(git rev-parse --short HEAD)
 
 # Create *new* git repo in html folder
-cd _build
-git init
-git config user.name "Rodrigo Luger"
-git config user.email "rodluger@gmail.com"
-
-# We will push to the pdf branch
-git remote add upstream "https://$GH_TOKEN@github.com/rodluger/cv.git"
-git fetch upstream && git reset upstream/pdf
-
-# Refresh the pdf and delete aux stuff
-touch cv.pdf
-rm -f cv.aux
-rm -f cv.log
-rm -f cv.out
-
-# Commit and push!
-git add cv.pdf
-git commit -m "rebuild pdf at ${rev}"
-git push -q -f upstream HEAD:pdf
+cd $TRAVIS_BUILD_DIR
+git checkout --orphan pdf
+git rm -rf .
+git add -f cv.pdf
+git -c user.name='travis' -c user.email='travis' commit -m "rebuild pdf at ${rev}"
+git push -q -f https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG pdf
